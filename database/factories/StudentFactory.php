228927f1557cfaba1bccffbb9a80bea369e2376a
userxamii <hannamaee.avancena@gmail.com>
2026-08-26 10:21:11 +0800

    showed json data

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
+            'email' => fake() ->unique() ->SafeEmail(),
+            'program' => fake() -> randomElement ([
+                'BSIT',
+                'BSCS',
+                'BSIS',
+            ]),
+            'gender' => fake() -> randomElement([
+                'female',
+                'male'
+            ]),
+            'birthday' => fake() 
+            ->dateTimeBetween ('-25 years', '-17 years')
+            ->format('Y-m-d'),
+            'yr_level' => fake() -> numberBetween(1,4),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Student;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Student::factory(2000)->create();
 
         User::factory()->create([
             'name' => 'Test User',
